36cac6042baee94e75057d0c35479b1fe54ba9fc
Daan de Waard <daan.de.waard@hz.nl>
2021-11-10 16:24:31 +0100

    Update config to match latest ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,67 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
-    'import'
+    'import',
+    'jsdoc'
   ],
   extends: [
     'airbnb-base',
     'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsdoc/recommended'
   ],
   parserOptions: {
       project: './tsconfig.json',
   },
   rules: {
-    'no-console': 0
+    'no-console': 0,
+    'jsdoc/require-jsdoc': [
+      'error', 
+      {
+        'contexts': [
+          'MethodDefinition:not([accessibility="private"])'
+        ],
+        'require': {
+          'ClassDeclaration': false,
+          'MethodDefinition': false
+        },
+      }
+    ],
+    'jsdoc/require-param-type': 0,
+    'jsdoc/no-types': [
+      'warn',
+      {
+        'contexts': ['any']
+      } 
+    ],
+    'camelcase': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'default',
+        'format': ['camelCase']
+      },
+  
+      {
+        'selector': 'variable',
+        'format': ['camelCase', 'UPPER_CASE']
+      },
+      {
+        'selector': 'parameter',
+        'format': ['camelCase'],
+        'leadingUnderscore': 'allow'
+      },
+  
+      {
+        'selector': 'memberLike',
+        'modifiers': ['private'],
+        'format': ['camelCase']
+      },
+  
+      {
+        'selector': 'typeLike',
+        'format': ['PascalCase']
+      }
+    ]  
   }
 };
