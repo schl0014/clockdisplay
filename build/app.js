c8b794b8329333780ff763d5cb533fcba6eb46f0
Daan de Waard <daan.de.waard@hz.nl>
2021-11-10 16:26:42 +0100

    Add built files

--- build/app.js
+++ build/app.js
@@ -1,25 +1,52 @@
-class Animal {
-    name;
-    legs;
-    sound;
-    constructor(name, legs, sound) {
-        this.name = name;
-        this.legs = legs;
-        this.sound = sound;
+class ClockDisplay {
+    hours;
+    minutes;
+    output;
+    constructor(output) {
+        this.output = output;
+        this.hours = 0;
+        this.minutes = 0;
+        this.updateDisplay();
     }
-    getName() {
-        return this.name;
+    timeTick() {
+        this.minutes = (this.minutes + 1) % 60;
+        if (this.minutes === 0) {
+            this.hours = (this.hours + 1) % 24;
+        }
+        this.updateDisplay();
     }
-    getLegs() {
-        return this.legs;
+    setTime(hours, minutes) {
+        this.hours = Number(hours);
+        this.minutes = Number(minutes);
+        this.updateDisplay();
     }
-    getSound() {
-        return this.sound;
+    updateDisplay() {
+        let displayString = '';
+        if (this.hours < 10) {
+            displayString += '0';
+        }
+        displayString += `${this.hours}:`;
+        if (this.minutes < 10) {
+            displayString += '0';
+        }
+        displayString += this.minutes;
+        this.output.innerText = displayString;
     }
 }
-const animals = [
-    new Animal('dog', 4, 'woof'),
-    new Animal('cat', 4, 'meow'),
-];
-animals.forEach((animal) => console.log('A %s has %s legs and goes %s!', animal.getName(), animal.getLegs(), animal.getSound()));
+console.log('Javascript is working!');
+window.addEventListener('load', () => {
+    console.log('Handling the Load event');
+    const outputHTMLElement = document.getElementById('output');
+    const hoursInput = document.getElementById('hoursInput');
+    const minutesInput = document.getElementById('minutesInput');
+    const clock = new ClockDisplay(outputHTMLElement);
+    document.getElementById('tickerButton').addEventListener('click', () => {
+        console.log('User clicked ticker button');
+        clock.timeTick();
+    });
+    document.getElementById('setTime').addEventListener('click', () => {
+        console.log('User clicked setTime button');
+        clock.setTime(hoursInput.value, minutesInput.value);
+    });
+});
 //# sourceMappingURL=app.js.map
